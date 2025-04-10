b9e43685ab577f435dc5503f0fdb64f78239b9fc
Pandey <archana.pandey111295@gmail.com>
2025-04-10 16:02:04 +0200

    adding to my work

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function myFunction() {
       x.style.display = "block";
     }
   }
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      // const slide = entry.target.querySelector('.slide');
+      const slide = entry.target;
+  
+      if (entry.isIntersecting) {
+        slide.classList.add('wipe-enter');
+      return; // if we added the class, exit the function
+      }
+  
+      // We're not intersecting, so remove the class!
+      slide.classList.remove('wipe-enter');
+    });
+  });
+  
+  observer.observe(document.querySelector('.tools'));
